cdc5c88eefcfbad38ab81fffa5dcc129173f6a3f
TheSayvior <Nevatarion@hotmail.com>
2016-04-08 23:48:00 +0200

    Added 1. version movement control - change the crazy lack of drag

---
+++ New Unity Project 1/Assets/Scritps/Movement/CameraLockOnObjectScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLockOnObjectScript : MonoBehaviour {
+
+
+    public GameObject Drone;
+    public float DistanceFromDrone;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Drone.transform.position + new Vector3(0, 0, -DistanceFromDrone);
+    }
+}
---
+++ New Unity Project 1/Assets/Scritps/Movement/DroneMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DroneMovement : MonoBehaviour {
+
+    public Rigidbody2D Drone, LeftThruster, RightThruster;
+
+    public float Acceleration = 2f;
+    public float Drag = 0.2f;
+    public float AngularDrag = 50;
+
+    private bool _thrustingLeft;
+    private bool _thrustingRight;
+
+    void Start () {
+        Drone.drag = Drag;
+        Drone.mass = 1;
+        LeftThruster.drag = Drag;
+        LeftThruster.mass = 1;
+        RightThruster.drag = Drag;
+        RightThruster.mass = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _thrustingLeft = Input.GetKey(KeyCode.A);
+        _thrustingRight = Input.GetKey(KeyCode.D);
+    }
+
+    void FixedUpdate () {
+        if (_thrustingLeft)
+            LeftThruster.AddForce(LeftThruster.transform.up * Acceleration, ForceMode2D.Force);
+        if (_thrustingRight)
+            RightThruster.AddForce(RightThruster.transform.up * Acceleration, ForceMode2D.Force);
+    }
+}
