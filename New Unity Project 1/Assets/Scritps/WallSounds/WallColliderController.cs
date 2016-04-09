29cb8cf7ca8ad540d26ed10af96a45b28a9e9964
ChristofferL <chrwadum@gmail.com>
2016-04-09 23:10:40 +0200

    Sounds when hitting wall

---
+++ New Unity Project 1/Assets/Scritps/WallSounds/WallColliderController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class WallColliderController : MonoBehaviour {
+
+	public AudioClip[] Sounds;
+
+	private AudioSource _source;
+
+	void Start()
+	{
+		_source = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Hit!");
+		if(other.tag == "Player" && !_source.isPlaying)
+		{
+			_source.clip = Sounds[Random.Range(0,Sounds.Length-1)];
+			_source.Play();
+		}
+	}
+}
