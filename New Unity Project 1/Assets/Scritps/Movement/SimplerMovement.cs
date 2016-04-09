129269bcaedfe0d9905a553fcc1a5aaf630804f3
tskaiser <lazareth@gmail.com>
2016-04-09 19:53:42 +0200

    \o/ movement "works!"

---
+++ New Unity Project 1/Assets/Scritps/Movement/SimplerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SimplerMovement : MonoBehaviour {
+
+    public KeyCode LeftThruster;
+    public KeyCode RightThruster;
+
+    public float ForwardForce;
+    public float RotationForce;
+
+    private Rigidbody2D _drone;
+
+    private bool _leftThrusterOn;
+    private bool _rightThrusterOn;
+
+    private void Start() {
+        _drone = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update () {
+        _leftThrusterOn = Input.GetKey(LeftThruster);
+        _rightThrusterOn = Input.GetKey(RightThruster);
+    }
+
+    private void FixedUpdate() {
+        if (!_leftThrusterOn && !_rightThrusterOn) {
+            return;
+        }
+
+        _drone.AddForce(ForwardForce*_drone.transform.up*Time.fixedDeltaTime, ForceMode2D.Force);
+
+        if (_leftThrusterOn && !_rightThrusterOn)
+            _drone.AddTorque(RotationForce*Time.fixedDeltaTime, ForceMode2D.Force);
+
+        if (_rightThrusterOn && !_leftThrusterOn)
+            _drone.AddTorque(-RotationForce*Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+}
