a79047f27b0f47e77abe10559323ec04b308053b
TheSayvior <Nevatarion@hotmail.com>
2016-04-10 00:46:24 +0200

    reactor

--- New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
+++ New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class HookControl : MonoBehaviour {
 
-    void OnTriggerEnter2D(Collider2D other)
+    //Works
+    /*void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "HookableObject")
         {
             other.transform.parent = this.transform;
+        }
+    }*/
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "HookableObject")
+        {
+            other.GetComponent<HingeJoint2D>().connectedBody = this.transform.parent.gameObject.GetComponent<Rigidbody2D>();
+            other.GetComponent<HingeJoint2D>().enabled = true;
         }
     }
 }
