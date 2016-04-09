13f3521054b1bff92f009af2a7bdc69801edc53a
TheSayvior <Nevatarion@hotmail.com>
2016-04-09 20:38:41 +0200

    Hook completed

--- New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
+++ New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
@@ -3,28 +3,9 @@ using System.Collections;
 
 public class HookControl : MonoBehaviour {
 
-    public RopeController Rope;
-
-    private HingeJoint2D _hook;
-
-    private bool _FreeHook = true;
-
-    void start()
-    {
-        _hook = this.GetComponent<HingeJoint2D>();
-    }
-
-    void FixedUpdate()
-    {
-
-       // this.transform.position = Rope.RopeLinks[Rope.RopeDistance].transform.position;
-       //_hook.connectedBody = Rope.RopeLinks[Rope.RopeDistance-].gameObject.GetComponent<Rigidbody2D>();
-
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "HookableObject" && _FreeHook)
+        if (other.gameObject.tag == "HookableObject")
         {
             other.transform.parent = this.transform;
         }
