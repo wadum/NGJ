dd21cded7dcd2c0c16c972880f36b8bd25eae478
ChristofferL <chrwadum@gmail.com>
2016-04-10 11:52:23 +0200

    TitleScene

---
+++ New Unity Project 1/Assets/LoadGameScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGameScene : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown){
+			UnityEngine.SceneManagement.SceneManager.LoadScene("CobotsGrab");
+		}
+	}
+}
