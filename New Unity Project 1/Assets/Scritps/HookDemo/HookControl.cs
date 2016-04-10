05125165cc79d78895e8a6ba9d80d294d28d285c
TheSayvior <Nevatarion@hotmail.com>
2016-04-10 11:12:09 +0200

    no message

--- New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
+++ New Unity Project 1/Assets/Scritps/HookDemo/HookControl.cs
@@ -19,15 +19,49 @@ public class HookControl : MonoBehaviour {
         {
             other.GetComponent<HingeJoint2D>().connectedBody = this.transform.parent.gameObject.GetComponent<Rigidbody2D>();
             other.GetComponent<HingeJoint2D>().enabled = true;
-            if(this.gameObject.transform.root.name == "HappyBot")
+            Debug.Log(this.gameObject.transform.root.gameObject.name);
+            if (this.gameObject.transform.root.gameObject.name == "PlayerAWD")
             {
-                SoundFiles.PlayMagnet_Happy();
+                int randomNumber = Random.Range(0, 2);
+                StartCoroutine(HappyGrabbingConversation(randomNumber));               
             }
 
-            if (this.gameObject.transform.root.name == "AngryBot")
+            if (this.gameObject.transform.root.gameObject.tag == "PlayerArrows")
             {
-                SoundFiles.PlayMagnet_Angry();
+                int randomNumber = Random.Range(0, 2);
+                StartCoroutine(AngryGrabbingConversation(randomNumber));
             }
         }
+    }
+
+    IEnumerator HappyGrabbingConversation(int RN)
+    {
+        Debug.Log("Play sound");
+        if (RN == 0) SoundFiles.PlayH_Talk_1();
+        if (RN == 1) SoundFiles.PlayH_Talk_2();
+        if (RN == 2) SoundFiles.PlayH_Talk_3();
+
+        yield return new WaitForSeconds(1.5f);
+
+        if (RN == 0) SoundFiles.PlayA_Talk_1();
+        if (RN == 1) SoundFiles.PlayA_Talk_2();
+        if (RN == 2) SoundFiles.PlayA_Talk_3();
+
+        yield return null;
+    }
+
+    IEnumerator AngryGrabbingConversation(int RN)
+    {
+        if (RN == 0) SoundFiles.PlayA_Talk_1();
+        if (RN == 1) SoundFiles.PlayA_Talk_2();
+        if (RN == 2) SoundFiles.PlayA_Talk_3();
+
+        yield return new WaitForSeconds(1.5f);
+
+        if (RN == 0) SoundFiles.PlayH_Talk_1();
+        if (RN == 1) SoundFiles.PlayH_Talk_2();
+        if (RN == 2) SoundFiles.PlayH_Talk_3();
+
+        yield return null;
     }
 }
