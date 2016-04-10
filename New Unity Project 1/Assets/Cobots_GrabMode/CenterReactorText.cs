e3954cbb1008758e20ec631bf9d290329d4d365d
TheSayvior <Nevatarion@hotmail.com>
2016-04-10 13:11:10 +0200

    no message

--- New Unity Project 1/Assets/Cobots_GrabMode/CenterReactorText.cs
+++ New Unity Project 1/Assets/Cobots_GrabMode/CenterReactorText.cs
@@ -7,20 +7,17 @@ public class CenterReactorText : MonoBehaviour {
 	public GameObject CenterObject;
     public SpriteRenderer sprite;
 
-    public GameObject leak1, leak2;
 
     private Text text;
 	private ReactorState reactor;
 	private RectTransform rect;
-    private ParticleEmitter particle1, particle2;
 
 	void Awake()
 	{
 		rect = GetComponent<RectTransform>();
 		text = GetComponent<Text>();
 		reactor = FindObjectOfType<ReactorState>();
-        /*particle1 = leak1.GetComponent<ParticleEmitter>();
-        particle2 = leak2.GetComponent<ParticleEmitter>();*/
+      
     }
 
 	// Update is called once per frame
@@ -30,7 +27,6 @@ public class CenterReactorText : MonoBehaviour {
 		rect.position = Camera.main.WorldToScreenPoint(CenterObject.transform.position);
 		rect.rotation = CenterObject.transform.rotation;
 
-        /*particle1.maxEmission = reactor.ReactorMaxHealth * 2;
-        particle2.maxEmission = reactor.ReactorMaxHealth * 2;*/
+       
     }
 }
